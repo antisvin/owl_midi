6a246b236730e486962ec11eb7cd04ea6f8b9260
antisvin <antisvin@gmail.com>
2022-01-07 00:33:31 +0300

    Added MIDI status bindings too

--- build.rs
+++ build.rs
@@ -14,7 +14,7 @@ fn main() {
         // bindings for.
         .header("OpenWareMidiControl.h")
         .prepend_enum_name(false)        
-        //.newtype_enum("PatchButtonId")
+        .fit_macro_constants(true)
         .rustified_enum("PatchButtonId")
         .rustified_enum("PatchParameterId")
         .rustified_enum("OpenWareMidiControl")
@@ -32,4 +32,27 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+    let status = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("MidiStatus.h")
+        .prepend_enum_name(false)  
+        .translate_enum_integer_types(true)
+        .newtype_enum("MidiStatus")
+        .newtype_enum("MidiControlChange")
+        .newtype_enum("UsbMidi")
+        .newtype_enum("OwlProtocol")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    status
+        .write_to_file(out_path.join("status.rs"))
+        .expect("Couldn't write status!");
 }
