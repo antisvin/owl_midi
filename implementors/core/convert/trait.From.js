3efc30f74f63d69d73b989f5129aaebad84e7c79
antisvin <antisvin@users.noreply.github.com>
2022-05-01 16:08:19 +0000

    deploy: f4d2dcd19786721af7eb9c752d28aca87e5072a2

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["owl_midi"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.60.0/std/primitive.isize.html\">isize</a>&gt; for <a class=\"enum\" href=\"owl_midi/enum.SysexConfiguration.html\" title=\"enum owl_midi::SysexConfiguration\">SysexConfiguration</a>","synthetic":false,"types":["owl_midi::SysexConfiguration"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
