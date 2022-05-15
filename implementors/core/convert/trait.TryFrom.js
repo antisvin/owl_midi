07aa6eedafe6e60272ec3b2c789548458c9f1f97
antisvin <antisvin@users.noreply.github.com>
2022-05-15 11:45:43 +0000

    deploy: add6a35716499668b4478bdd1749fbea18cb82a5

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["owl_midi"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"enum\" href=\"owl_midi/enum.OpenWareMidiControl.html\" title=\"enum owl_midi::OpenWareMidiControl\">OpenWareMidiControl</a>&gt; for <a class=\"enum\" href=\"owl_midi/enum.PatchParameterId.html\" title=\"enum owl_midi::PatchParameterId\">PatchParameterId</a>","synthetic":false,"types":["owl_midi::PatchParameterId"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
